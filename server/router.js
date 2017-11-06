552a0a7dd2fe3d4de60c7791b7e27b98746fc88f
njs6145 <njs6145@g.rit.edu>
2017-11-06 16:31:38 -0500

    fixed typo

--- server/router.js
+++ server/router.js
@@ -13,7 +13,7 @@ const router = app => {
   app.get('/', controllers.Account.loginPage);
   */
 
-  app.get('/login', mid.requiresSecure, mid.requiresLogout, mid.controllers.Account.loginPage);
+  app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
   app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
   app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
