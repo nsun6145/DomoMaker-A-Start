7dd5dc726e374de3a8faf64a7ed5e901632e1478
Nathaniel Sun <njs6145@g.rit.edu>
2017-11-10 00:22:46 -0500

    moved files around

--- server/router.js
+++ server/router.js
@@ -2,21 +2,10 @@ const controllers = require('./controllers');
 const mid = require('./middleware');
 
 const router = app => {
-  /*
-  app.get('/login', controllers.Account.loginPage);
-  app.post('/login', controllers.Account.login);
-  app.get('/signup', controllers.Account.signupPage);
-  app.post('/signup', controllers.Account.signup);
-  app.get('/logout', controllers.Account.logout);
-  app.get('/maker', controllers.Domo.makerPage);
-  app.post('/maker', controllers.Domo.make);
-  app.get('/', controllers.Account.loginPage);
-  */
   app.get('/getToken', mid.requiresSecure, controllers.Account.getToken);
   app.get('/getDomos', mid.requireLogin, controllers.Domo.getDomos);
   app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
-  // app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
   app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
   app.get('/logout', mid.requiresLogin, controllers.Account.logout);
   app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
